51d9686ba22bf0406abba6269eed1ef5a0744887
Gobi <ohhj1999@gmail.com>
2023-01-15 21:14:05 +0900

    feat : Add 시작페이지, 마이페이지 구성

--- UI/lib/pages/start_page.dart
+++ UI/lib/pages/start_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ui/constants.dart';
 import 'package:ui/pages/home_page.dart';
+import 'package:ui/widgets/custom_header.dart';
 import 'package:ui/widgets/footer.dart';
-import 'package:ui/widgets/signin_header.dart';
-import 'package:ui/widgets/start_header.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key});
@@ -16,43 +15,53 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     var _width = MediaQuery.of(context).size.width;
-    return SafeArea(
-        minimum: const EdgeInsets.all(60),
-        child: Scaffold(
-            body: Column(
-          children: [
-            startHeader(context),
-            defaultSpacer,
-            // 서비스 개요
-            Container(
-                decoration: outerBorder,
-                height: 500,
-                width: _width,
-                child: const Expanded(child: Center(child: Text('서비스개요')))),
-            defaultSpacer,
-            Container(
-                decoration: outerBorder,
-                // BoxConstraints has non-normalized width constraints.
-                // The offending constraints were:
-                //   BoxConstraints(64.0<=w<=45.0, 36.0<=h<=80.0; NOT NORMALIZED)
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: Size(_width * 0.2, buttonHeight),
-                      backgroundColor: kLightGrey,
-                      side: noBorder,
-                      padding: const EdgeInsets.all(16)),
-                  child: Text('시작하기',
-                      style: TextStyle(
-                          fontSize: defaultFontSize, color: kDarkGrey)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HomePage()));
-                    debugPrint('시작하기');
-                  },
-                )),
-            const Spacer(),
-            footer(),
-          ],
-        )));
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.all(60),
+            child: Column(
+              children: [
+                startHeader(context),
+                defaultSpacer,
+                // 서비스 개요
+                Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    Container(
+                        decoration: outerBorder,
+                        height: 600,
+                        width: _width,
+                        child: Center(
+                            child: Text('서비스개요', style: titleTextStyle))),
+                    defaultSpacer,
+                    Container(
+                        margin: EdgeInsets.all(30),
+                        height: titleHeight * 1.5,
+                        width: buttonWidth * 2,
+                        decoration: outerBorder,
+                        child: ElevatedButton(
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor: kWhite,
+                              side: whiteBorder,
+                              padding: const EdgeInsets.all(16)),
+                          child: Text('시작하기',
+                              style: TextStyle(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                            debugPrint('시작하기');
+                          },
+                        )),
+                  ],
+                ),
+                const Spacer(),
+                footer(),
+              ],
+            )));
   }
 }
--- UI/lib/pages/user_page.dart
+++ UI/lib/pages/user_page.dart
@@ -1,21 +1,152 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:ui/widgets/signin_header.dart';
+import 'package:ui/constants.dart';
+import 'package:ui/widgets/footer.dart';
+import 'package:ui/widgets/custom_header.dart';
+import 'package:ui/widgets/sample_card.dart';
+import 'package:ui/widgets/titlebar.dart';
 
 class UserPage extends StatefulWidget {
-  const UserPage({super.key});
-
   @override
   _UserPageState createState() => _UserPageState();
 }
 
 class _UserPageState extends State<UserPage> {
+  get follower => 5;
+  get following => 5;
+  get match => 98;
+  var name = '고비';
+  bool isMyPage = true;
+
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        body: Column(
-      children: [
-        signinHeader(context),
-      ],
-    ));
+        body: Padding(
+            padding: const EdgeInsets.all(60),
+            child: Column(children: [
+              customHeader(context, '메인페이지'),
+              defaultSpacer,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      titleBar(width * 0.3, '내 정보', withArrow: false),
+                      Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: outerBorder,
+                          height: 280,
+                          width: width * 0.3,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: profileBorder,
+                                  height: 200,
+                                  width: 200,
+                                  child: const CircleAvatar(
+                                    backgroundImage:
+                                        AssetImage('assets/image.jpg'),
+                                  ),
+                                ),
+                                defaultSpacer,
+                                defaultSpacer,
+                                Stack(
+                                  alignment: AlignmentDirectional.bottomStart,
+                                  children: [
+                                    SizedBox(
+                                      // decoration: outerBorder,
+                                      width: width * 0.12,
+                                      height: 180,
+                                      child: GridView.count(
+                                          crossAxisCount:
+                                              2, //1 개의 행에 보여줄 item 개수
+                                          childAspectRatio: (1 / 0.3),
+                                          crossAxisSpacing: 10,
+                                          children: [
+                                            Text('$name 님',
+                                                style: titleTextStyle),
+                                            const Text(''),
+                                            Text('♥ 팔로잉',
+                                                style: contentsTextStyle),
+                                            Text(
+                                              '$following 명',
+                                              style: contentsTextStyle,
+                                              textAlign: TextAlign.end,
+                                            ),
+                                            Text('♥ 팔로워',
+                                                style: contentsTextStyle),
+                                            Text(
+                                              '$follower 명',
+                                              style: contentsTextStyle,
+                                              textAlign: TextAlign.end,
+                                            ),
+                                            Text('♥ 취향매칭률',
+                                                style: contentsTextStyle),
+                                            Text(
+                                              '$match %',
+                                              style: contentsTextStyle,
+                                              textAlign: TextAlign.end,
+                                            ),
+                                          ]),
+                                    ),
+                                    Container(
+                                        height: titleHeight * 0.8,
+                                        width: buttonWidth * 2,
+                                        child: ElevatedButton(
+                                          style: OutlinedButton.styleFrom(
+                                            backgroundColor: Colors.transparent,
+                                            side: whiteBorder,
+                                          ),
+                                          child: Text('팔로우',
+                                              style: contentsTextStyle),
+                                          onPressed: () {},
+                                        )),
+                                  ],
+                                ),
+                              ])),
+                      defaultSpacer,
+                      titleBar(width * 0.3, '나의 재생목록'),
+                      Container(
+                          decoration: outerBorder,
+                          width: width * 0.3,
+                          height: 280,
+                          child: ListView.builder(
+                            padding: defaultPadding,
+                            scrollDirection: Axis.horizontal,
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: 10,
+                            itemBuilder: (BuildContext context, int index) {
+                              return playlistCard();
+                            },
+                          )),
+                    ],
+                  ),
+                  const Spacer(),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        titleBar(width * 0.6, '$name님의 취향분석 결과',
+                            withArrow: false),
+                        Container(
+                            decoration: outerBorder,
+                            width: width * 0.6,
+                            height: 625,
+                            child: Center(
+                                child: Text(
+                              '취향분석 결과',
+                              style: titleTextStyle,
+                            )))
+                      ]),
+                ],
+              ),
+              const Spacer(),
+              footer()
+            ])));
   }
 }
