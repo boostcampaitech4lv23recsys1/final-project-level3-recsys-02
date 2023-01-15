0ca9495a798a8d0bf356eaf907c04a38003e2fb6
Gobi <ohhj1999@gmail.com>
2023-01-15 21:12:15 +0900

    feat : Add 테스트이미지, 세팅

--- UI/lib/constants.dart
+++ UI/lib/constants.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:ui/models/item.dart';
+
+BorderRadius kBorder = BorderRadius.circular(12);
 
 Color kLightGrey = const Color(0xFFE5E5E5);
 Color kDarkGrey = const Color.fromARGB(255, 155, 155, 155);
 
+Color kBlack = const Color.fromRGBO(22, 22, 22, 1);
+Color kWhite = const Color.fromARGB(255, 240, 240, 240);
+
+EdgeInsets outerMargin = const EdgeInsets.all(20);
+EdgeInsets defaultPadding = const EdgeInsets.all(8);
 const kPadding = EdgeInsets.only(top: 8, bottom: 12, left: 8, right: 8);
-const kBorderRadius = 8.0;
 
-const contentsFontSize = 16.0;
-const defaultFontSize = 18.0;
-const titleFontSize = 20.0;
+final defaultTextStyle = TextStyle(color: kWhite, fontSize: 12.0);
+final contentsTextStyle = TextStyle(color: kWhite, fontSize: 14.0);
+final subtitleTextStyle = TextStyle(
+  color: kWhite,
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+);
+final titleTextStyle = TextStyle(
+  color: kWhite,
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+);
 
-const buttonHeight = 45.0;
+const titleHeight = 45.0;
+const buttonWidth = 120.0;
 
 const Widget defaultSpacer = SizedBox(
   width: 20,
   height: 20,
 );
-BorderSide noBorder = const BorderSide(
-  width: 0,
-  style: BorderStyle.none,
+BorderSide whiteBorder =
+    BorderSide(color: kWhite, strokeAlign: StrokeAlign.outside);
+
+BoxDecoration profileBorder = BoxDecoration(
+  border: Border.all(color: Colors.white, width: 1.5),
+  shape: BoxShape.circle,
 );
-BoxDecoration outerBorder =
-    BoxDecoration(border: Border.all(color: Colors.red));
 
-EdgeInsets outerMargin = const EdgeInsets.all(20);
-EdgeInsets defaultPadding = const EdgeInsets.all(8);
-
-List<Item> itemList = [
-  Item(track: 0, trackName: '가', artist: 0, artistName: 'artistName'),
-  Item(track: 1, trackName: '나', artist: 0, artistName: 'artistName'),
-  Item(track: 2, trackName: '다', artist: 0, artistName: 'artistName'),
-  Item(track: 3, trackName: '라', artist: 0, artistName: 'artistName'),
-  Item(track: 4, trackName: '마', artist: 0, artistName: 'artistName'),
-  Item(track: 5, trackName: '바', artist: 0, artistName: 'artistName'),
-  Item(track: 6, trackName: '사', artist: 0, artistName: 'artistName'),
-  Item(track: 7, trackName: '아', artist: 0, artistName: 'artistName'),
-  Item(track: 8, trackName: '자', artist: 0, artistName: 'artistName'),
-  Item(track: 9, trackName: '차', artist: 0, artistName: 'artistName'),
-];
+BoxDecoration outerBorder = BoxDecoration(
+    border: Border.all(color: Colors.white, width: 1.5),
+    shape: BoxShape.rectangle,
+    borderRadius: const BorderRadius.all(Radius.circular(8)));
--- UI/lib/main.dart
+++ UI/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ui/constants.dart';
-import 'package:ui/pages/signin_page.dart';
+import 'package:ui/pages/home_page.dart';
+import 'package:ui/pages/login_page.dart';
+import 'package:ui/pages/signin/signin_page.dart';
 import 'package:ui/pages/start_page.dart';
-
-import 'pages/home_page.dart';
+import 'package:ui/pages/user_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +15,119 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: kLightGrey, // 강조색
+        scaffoldBackgroundColor: kBlack, // 앱 배경색
+      ),
       title: 'Final Project',
-      home: StartPage(),
+      home: TestPage(),
       debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                        child: Text('--- 페이지 목록 ---', style: titleTextStyle))),
+                defaultSpacer,
+                ElevatedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: kWhite,
+                      side: whiteBorder,
+                      padding: const EdgeInsets.all(16)),
+                  child: Text('홈페이지',
+                      style: TextStyle(
+                        color: kBlack,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                ),
+                defaultSpacer,
+                ElevatedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: kWhite,
+                      side: whiteBorder,
+                      padding: const EdgeInsets.all(16)),
+                  child: Text('로그인페이지',
+                      style: TextStyle(
+                        color: kBlack,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                ),
+                defaultSpacer,
+                ElevatedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: kWhite,
+                      side: whiteBorder,
+                      padding: const EdgeInsets.all(16)),
+                  child: Text('회원가입페이지',
+                      style: TextStyle(
+                        color: kBlack,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SigninPage()));
+                  },
+                ),
+                defaultSpacer,
+                ElevatedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: kWhite,
+                      side: whiteBorder,
+                      padding: const EdgeInsets.all(16)),
+                  child: Text('시작페이지',
+                      style: TextStyle(
+                        color: kBlack,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => StartPage()));
+                  },
+                ),
+                defaultSpacer,
+                ElevatedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: kWhite,
+                      side: whiteBorder,
+                      padding: const EdgeInsets.all(16)),
+                  child: Text('마이페이지',
+                      style: TextStyle(
+                        color: kBlack,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => UserPage()));
+                  },
+                ),
+              ],
+            )));
   }
 }
