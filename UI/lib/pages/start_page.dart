f81afcdb697b876cb87ca5f9769169fb35f25cfd
Gobi <ohhj1999@gmail.com>
2023-01-23 17:06:31 +0900

    feat: 로그인 여부에 따라 다르게 설정

--- UI/lib/pages/start_page.dart
+++ UI/lib/pages/start_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ui/constants.dart';
+import 'package:ui/pages/login_page.dart';
 import 'package:ui/widgets/custom_header.dart';
 import 'package:ui/widgets/footer.dart';
 
@@ -11,16 +13,122 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  var name;
+
+  void navigationPage() {
+    name == ''
+        ? {Navigator.pushNamed(context, '/main')}
+        : Navigator.pushNamed(context, '/main');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getSession();
+  }
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
+
+    Widget startHeader() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          MaterialButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/main');
+              },
+              child: Container(
+                  width: 180,
+                  height: titleHeight,
+                  color: kWhite,
+                  child: Image.asset(
+                    'assets/logo2.png',
+                  ))),
+          const Spacer(),
+          isLogin
+              ? Row(children: [
+                  Container(
+                      height: titleHeight,
+                      width: buttonWidth,
+                      decoration: outerBorder,
+                      child: ElevatedButton(
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            side: whiteBorder,
+                            padding: const EdgeInsets.all(16)),
+                        child: Text('메인페이지', style: subtitleTextStyle),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/main');
+                        },
+                      )),
+                  defaultSpacer,
+                  Container(
+                      height: titleHeight,
+                      width: buttonWidth,
+                      child: ElevatedButton(
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            side: whiteBorder,
+                            padding: const EdgeInsets.all(16)),
+                        child: Text('로그아웃', style: subtitleTextStyle),
+                        onPressed: () {
+                          setSession(false);
+                          setState(() {});
+                          Navigator.popUntil(
+                              context, ModalRoute.withName('/home'));
+                        },
+                      ))
+                ])
+              : Row(
+                  children: [
+                    Container(
+                        height: titleHeight,
+                        width: buttonWidth,
+                        decoration: outerBorder,
+                        child: ElevatedButton(
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              side: whiteBorder,
+                              padding: const EdgeInsets.all(16)),
+                          child: Text('로그인', style: subtitleTextStyle),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              PageRouteBuilder(
+                                opaque: false, // set to false
+                                pageBuilder: (_, __, ___) => LoginPage(),
+                              ),
+                            );
+                          },
+                        )),
+                    defaultSpacer,
+                    Container(
+                        height: titleHeight,
+                        width: buttonWidth,
+                        decoration: outerBorder,
+                        child: ElevatedButton(
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              side: whiteBorder,
+                              padding: const EdgeInsets.all(16)),
+                          child: Text('회원가입', style: subtitleTextStyle),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/signin');
+                          },
+                        ))
+                  ],
+                ),
+        ],
+      );
+    }
+
     return Scaffold(
         body: Padding(
             padding: outerPadding,
             child: Column(
               children: [
-                startHeader(context),
-                defaultSpacer,
+                startHeader(), defaultSpacer,
                 // 서비스 개요
                 Stack(
                   alignment: Alignment.bottomRight,
