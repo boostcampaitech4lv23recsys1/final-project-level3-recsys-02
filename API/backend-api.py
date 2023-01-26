559b78cccd290fb6ed0c3a67291678e5ee781562
yhw991228 <yhw991228@gmail.com>
2023-01-26 16:29:05 +0900

    rebase

--- API/backend-api.py
+++ API/backend-api.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 import uvicorn
 import pandas as pd
@@ -7,7 +6,6 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime, timezone
 
-
 app = FastAPI()
 
 # Fail response
@@ -15,8 +13,10 @@ NAME_NOT_FOUND = HTTPException(status_code=400, detail="Name not found.")
 TRACK_NOT_FOUND = HTTPException(status_code=400, detail="Track not found.")
 AuthError = HTTPException(status_code=400, detail="not auth user")
 
+
 def change_str(a):
     return a.replace('\'', '')
+
 
 class trackInfo(BaseModel):
     track_name: str
@@ -26,6 +26,7 @@ class trackInfo(BaseModel):
     date_uts: int
     artist_name: str
 
+
 class ops(BaseModel):
     option: int
 
@@ -34,28 +35,30 @@ class User(BaseModel):
     id: str
     pwd: str
 
+
 class userInfo(BaseModel):
-    name : str # id
+    name: str  # id
     password: str
-    realname : str
-    image : str
-    country : str
-    age : int
-    gender : int
-    playcount : int
-    following : list[str]
-    follower : list[str]
+    realname: str
+    image: str
+    country: str
+    age: int
+    gender: int
+    playcount: int
+    following: list[str]
+    follower: list[str]
 
 
 @app.post('/login', description='로그인')
-def login_user(user:  User) -> userInfo:
+def login_user(user: User) -> userInfo:
     query = f"SELECT password FROM user_info WHERE user_name ='{user.id}';"
     user_df = pd.read_sql(query, db_connect)
 
-    if user.pwd == user_df['password']: 
+    if user.pwd == user_df['password']:
         return userInfo()
     else:
         return '아이디 비밀번호 불일치'
+
 
 @app.post('/signin', description='회원가입')
 def signin_user(userInfo: userInfo, tags: list[str], artists: list[str]):
@@ -63,7 +66,7 @@ def signin_user(userInfo: userInfo, tags: list[str], artists: list[str]):
     user_query = f"INSERT INTO user_info (user_name, realname, password, age, gender, country, playcount, follower, following) \
             VALUES ('{userInfo.name}', '{userInfo.realname}', '{userInfo.password}', \
                 {userInfo.age}, {userInfo.gender}, '{userInfo.country}', \
-                {userInfo.playcount}, {userInfo.follower},{ userInfo.following}) \
+                {userInfo.playcount}, {userInfo.follower},{userInfo.following}) \
             RETURNING success;"
     response1 = pd.read_sql(user_query, db_connect)
 
@@ -89,7 +92,7 @@ def signin_user(userInfo: userInfo, tags: list[str], artists: list[str]):
     print(artist_tracks)
 
     timestamp = int(datetime.now().replace(tzinfo=timezone.utc).timestamp())
-    
+
     # user_tracks = pd.concat(tag_tracks, artist_tracks)
     user_tracks = artist_tracks
     for ut in user_tracks:
@@ -100,8 +103,9 @@ def signin_user(userInfo: userInfo, tags: list[str], artists: list[str]):
 
     return response1 and response2
 
+
 @app.get("/users/{user_id}/profiles", description="사용자 정보")
-async def get_profiles(user_id : str):
+async def get_profiles(user_id: str):
     get_sample = f"""
         select * from user_info where name = '{user_id}'
     ;"""
@@ -109,38 +113,40 @@ async def get_profiles(user_id : str):
     with db_connect.cursor() as cur:
         cur.execute(get_sample)
         values = cur.fetchall()[0]
-    
+
     info = userInfo()
     info.name = values[0]
     info.realname = values[2]
     info.following = values[10]
     info.follower = values[11]
-    info.open = values[18]
+    info.open = values[-1]
 
     return info
 
-@app.get("/users/{user_id}", description="공개 여부 전환")
-async def update_open(user_id : str, ex = 0):
-    get_sample = f"""
-        update user_info set open = {not ex} where name = '{user_id}';
-    ;"""
-
-    with db_connect.cursor() as cur:
-        cur.execute(get_sample)
-        info = db_connect.commit()
 
 @app.get("/users/{user_id}/likes", description="좋아요 리스트")
-async def get_likes(user_id : str):
+async def get_likes(user_id: str):
     get_sample = f"""
-        select track_name from inter where (user_id = '{user_id}' and Interaction = true)
+        select track_name from inter where (user_id = '{user_id}' and loved = 1)
     ;"""
 
     with db_connect.cursor() as cur:
         cur.execute(get_sample)
-        return cur.fetchall()
+        likes = cur.fetchall()
 
-@app.post("/users/{user_id}/profiles",description='내 정보, 프로필, 공개 여부,..')
-def get_User_info(input: userInfo):
+    get_sample = f"""
+        select track_name from inter where (user_id = '{user_id}' and loved = 2)
+    ;"""
+
+    with db_connect.cursor() as cur:
+        cur.execute(get_sample)
+        unlikes = cur.fetchall()
+
+    return list(set(likes) - set(unlikes))
+
+
+@app.post("/users/{user_id}/profiles", description='내 정보, 프로필, 공개 여부,..')
+def get_user_info(input: userInfo):
     user_name = input.name
     query = f"SELECT * FROM user_info WHERE name=\'{user_name}\'"
     df = pd.read_sql(query, db_connect)
@@ -149,19 +155,19 @@ def get_User_info(input: userInfo):
     else:
         return df
 
+
 @app.put("/users/{user_id}/likes/{track_name}", description='좋아요 목록')
 def add_interaction(input_1: userInfo, input_2: trackInfo, option: ops):
     timestamp = int(datetime.now().replace(tzinfo=timezone.utc).timestamp())
     if input_1.authentication == -1:
-        AuthError
+        return AuthError()
     else:
         query = f"INSERT INTO inter (track_name, loved, username, album_name, date_uts, artist_name)\
                  VALUES ('{change_str(input_2.track_name)}', {option.option}, '{input_1.name}', '{change_str(input_2.album_name)}', {timestamp}, '{input_2.artist_name}');"
-
         with db_connect.cursor() as cur:
             cur.execute(query)
             return "Success"
-
+        return "False"
 
 if __name__ == "__main__":
     db_connect = psycopg2.connect(
