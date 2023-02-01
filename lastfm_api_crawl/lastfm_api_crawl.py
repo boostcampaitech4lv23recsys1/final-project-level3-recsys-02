2b331fd60eba872b8df579ffc9db9055a3f4129f
dongyoung <ktasha45@gmail.com>
2023-02-01 01:04:24 +0000

    refactor: folder change

--- lastfm_api_crawl/lastfm_api_crawl.py
+++ lastfm_api_crawl/lastfm_api_crawl.py
@@ -26,9 +26,12 @@ dirname = f'ver_{args.ver}'
 if not os.path.isdir(dirname):
     os.makedirs(dirname)
 
-with open('./listeners_1.pkl', 'rb') as f:
-	username_list = pickle.load(f)
+# with open('./listeners_20k.npy', 'rb') as f:
+# 	username_list = pickle.load(f)
 
+username_list = np.load('./listeners_20k.npy')
+
+print(len(username_list))
 username_list = username_list[:args.user_sampling]
 
 url = 'http://ws.audioscrobbler.com/2.0'
