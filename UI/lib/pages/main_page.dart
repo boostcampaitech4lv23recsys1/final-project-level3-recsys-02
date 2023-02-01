f3e4cfd2a6095734c2a23859bfb0ffbd3b6a0b07
Gobi <ohhj1999@gmail.com>
2023-02-01 16:48:07 +0900

    feat: main app bar 이동

--- UI/lib/pages/main_page.dart
+++ UI/lib/pages/main_page.dart
@@ -27,10 +27,15 @@ class _MainPageState extends State<MainPage> {
 
   List<Item> musicList = [];
   List<Item> recList = [];
-  
+
   Future addInteraction(Item item) async {
-    await dioClient.interactionClick(username: username, albumName: item.albumName, artistName: item.artistName, trackName: item.trackName);
+    await dioClient.interactionClick(
+        username: username,
+        albumName: item.albumName,
+        artistName: item.artistName,
+        trackName: item.trackName);
   }
+
   void getMusicList() {
     for (int i = 0; i < 20; i++) {
       musicList.add(Item(
@@ -85,7 +90,6 @@ class _MainPageState extends State<MainPage> {
                             DetailPage(item: musicList[index]),
                       ),
                     );
-
                   },
                   child: trackCard(musicList[index],
                       isRank: true, index: index + 1));
@@ -116,7 +120,6 @@ class _MainPageState extends State<MainPage> {
                         opaque: false, // set to false
                         pageBuilder: (_, __, ___) =>
                             DetailPage(item: musicList[index]),
-
                       ),
                     );
                   },
@@ -160,6 +163,79 @@ class _MainPageState extends State<MainPage> {
             },
           ))
     ]);
+  }
+
+  AppBar mainAppBar(context) {
+    return AppBar(
+      toolbarHeight: 80,
+      elevation: 0,
+      backgroundColor: kBlack,
+      leading: ElevatedButton(
+        style: OutlinedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            padding: const EdgeInsets.all(12)),
+        child: Image.asset(
+          'assets/logo.png',
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, '/main');
+        },
+      ),
+      leadingWidth: 200,
+      title: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: TextField(
+            textAlign: TextAlign.center,
+            // controller: textcontroller,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+                hintText: '원하시는 노래를 검색하세요',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: whiteBorder),
+                filled: true,
+                contentPadding: const EdgeInsets.all(12),
+                fillColor: Colors.white,
+                prefixIcon: const Icon(Icons.search)),
+          )),
+      titleSpacing: 20,
+      actions: [
+        Container(
+          width: buttonWidth,
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          margin: const EdgeInsets.only(right: 10),
+          child: ElevatedButton(
+            style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                side: whiteBorder,
+                padding: const EdgeInsets.all(12)),
+            child: Text('마이페이지', style: subtitleTextStyle),
+            onPressed: () {
+              // list<str> = likesList;
+              // userInfo = profile;
+
+              Navigator.pushNamed(context, '/mypage');
+            },
+          ),
+        ),
+        Container(
+            width: buttonWidth,
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            margin: const EdgeInsets.only(right: 20),
+            child: ElevatedButton(
+              style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  side: whiteBorder,
+                  padding: const EdgeInsets.all(12)),
+              child: Text('로그아웃', style: subtitleTextStyle),
+              onPressed: () {
+                exitSession();
+                Navigator.popUntil(context, ModalRoute.withName('/home'));
+              },
+            )),
+      ],
+    );
   }
 
   @override
